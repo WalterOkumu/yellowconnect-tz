9a14c7d85f86a6ca4a0ab4a01915abfb9e2ee7a7
Helder Fonseca <helder2391@gmail.com>
2022-10-06 09:03:40 -0100

    add margin

--- components/Hero.js
+++ components/Hero.js
@@ -6,8 +6,8 @@ export default function Hero() {
 
     return(
         <div className="mx-auto max-w-6xl">
-            <div className="flex md:flex-row flex-col-reverse gap-8 md:mt-8">
-                <div className="md:mt-[4rem] md:w-1/2 text-center w-full">
+            <div className="flex md:flex-row flex-col-reverse gap-8 md:mt-2">
+                <div className="md:mt-[4rem] md:w-1/2 text-center w-full md:pt-6">
                     <h1 className="text-5xl font-bold md:text-left text-start md:w-9/12">Build strong digital presence!</h1>
                     <h2 className="text-4xl italic font-light pt-2 text-start md:text-left md:w-4/6">Get <span className="font-bold animate-blink">20%</span> off the first 3 months</h2>
                     <div className="pt-8">
@@ -16,7 +16,7 @@ export default function Hero() {
                             in this ever-changing market, YELLOW CONNECT is an important part 
                             of strategy to increase your visibility.
                         </p>
-                        <p className="leading-relaxed pt-4 text-justify">
+                        <p className="leading-relaxed text-justify">
                             With a good digital plan, your business can be on several online 
                             communication channels at the same time, facilitating your 
                             communication with your audience and achieving greater reach.
