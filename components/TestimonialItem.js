71047476619b7998c89587c4c74c8a1a5f457d9e
Helder Fonseca <helder2391@gmail.com>
2022-10-07 08:46:55 -0100

    fixing yarn build

--- components/TestimonialItem.js
+++ components/TestimonialItem.js
@@ -13,9 +13,9 @@ export default function TestimonialItem() {
             <div className="md:self-start">
                 <p  className="font-bold">José Manel</p>
                 <p className="text-xs text-gray-600">CEO da Blast Enterprise</p>
-                <p className="text-xs w-2/3 pt-1"><span className="text-[#FBED04]">"</span> Lorem ipsum dolor sit amet, 
+                <p className="text-xs w-2/3 pt-1"><span className="text-[#FBED04]"></span> Lorem ipsum dolor sit amet, 
                     consecterur adipiscing elit,
-                    sed do elusmod tempor<span className="text-[#FBED04]">"</span> 
+                    sed do elusmod tempor<span className="text-[#FBED04]"></span> 
                 </p>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -16,15 +16,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,100&display=swap" rel="stylesheet"></link>
         <link rel="icon" href="/favicon.ico" />
-        <Script
-          type="text/javascript"
-          strategy="beforeInteractive"
-          src="https://static.mailerlite.com/js/w/webforms.min.js?v7316d10e2931a97c7b0f5c7e7e7be3ea"
-          defer={false}
-        >
         
-            {function ml_webform_success_5786909(){let r=ml_jQuery||jQuery;r(".ml-subscribe-form-5786909 .row-success").show(),r(".ml-subscribe-form-5786909 .row-form").hide()}}
-        </Script>
       </Head>
 
       <main className='pb-10'>
@@ -36,6 +28,18 @@ export default function Home() {
         <Lead />
         <Testimonials />
       </main>
+
+
+      <Script
+          type="text/javascript"
+          strategy="beforeInteractive"
+          src="https://static.mailerlite.com/js/w/webforms.min.js?v7316d10e2931a97c7b0f5c7e7e7be3ea"
+          defer={false}
+          id="element"
+        >
+        
+            {function ml_webform_success_5786909(){let r=ml_jQuery||jQuery;r(".ml-subscribe-form-5786909 .row-success").show(),r(".ml-subscribe-form-5786909 .row-form").hide()}}
+        </Script>
       
     </div>
   )
